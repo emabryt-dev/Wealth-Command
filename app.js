737f1cb5c1af2372053f2d5de1b4c4c4cb81ee1f
emabryt-dev <emabryt@gmail.com>
2025-09-24 21:51:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -547,8 +547,7 @@ document.addEventListener("DOMContentLoaded", initApp);
 // Register Service Worker
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/service-worker.js')
+    .register('./service-worker.js')
     .then(reg => console.log('✅ SW registered:', reg.scope))
     .catch(err => console.error('❌ SW registration failed:', err));
 }
-
